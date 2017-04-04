070ff14c44bf6eea178bf26fb1cfb53fc9ec0ec4
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-04 12:04:38 -0700

    use debugger with for loop adding variables in watch

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 let a = 42; // global (file) scope
 let b = 1; // global (file) scope
 
-function bar () {
-    console.log('bar');
-    foo(0);
-}
 
-bar();
+let people = [
+    { name: 'Kevin', age: 21 },
+    { name: 'Ivy', age: 38 },
+    { name: 'Morgan', age: 32 },
+]
+
+let result = [];
+for (var i = 0; i < people.length; i++) {
+    debugger;
+    result.push(people[i].age);
+}
