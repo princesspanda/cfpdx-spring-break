d3aa010a6878a668c9032fd5541473fe694382a0
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-04 11:30:19 -0700

    debugger practice with more complicated scope

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
+let a = 42; // global (file) scope
+let b = 1; // global (file) scope
 
+begin = function (x) {
+    function adder() {
+        return a + b + x; // function scope
+    }
+    let sum = adder();
+    console.log(sum);
+    console.log("Hello, world!");    
+}
+begin(0);
+begin(1);
+begin(2);
+// function adder () {
+//     console.log(a + b); // function scope
+// }
+// let sum = adder();
+// console.log(sum);
+// console.log("Hello, world!");
 
-console.log("Hello, world!");
-debugger; // set a break point or do it in the browser (inspector -> sources tab)
-console.log('hello again');
+// Check the scope and call stack in the dev tools Sources tab
+// after setting a break point in the code.
+// Use step over, step into, and step out to go through lines
+// of code.
