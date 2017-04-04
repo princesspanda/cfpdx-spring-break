fa6aadca976dc31b83cc03f2acee7b73bb436969
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-04 12:28:22 -0700

    refactors for loop into an array.map

--- index.js
+++ index.js
@@ -8,8 +8,17 @@ let people = [
     { name: 'Morgan', age: 32 },
 ]
 
-let result = [];
-for (var i = 0; i < people.length; i++) {
-    debugger;
-    result.push(people[i].age);
-}
+/* refactored code from lines 21-24 */
+let result = people.map(function (anyNameYouWant, index) {
+    if (index % 2 === 0) {
+        return anyNameYouWant.age;
+    } 
+    // return people.age is 'undefined'
+})
+console.log('result is', result);
+
+/* the old way before array.map */
+// for (var i = 0; i < people.length; i++) {
+//     debugger;
+//     result.push(people[i].age);
+// }
