b805bfb69bd24f87193d3bed641442e32d32b750
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-05 10:03:17 -0700

    adds map practice

---
+++ map-practice.js
@@ -0,0 +1,13 @@
+const people = [
+    { firstName: 'Kevin', lastName: 'Wong', age: 21 },
+    { firstName: 'Ivy', lastName: 'Patton', age: 38 },
+    { firstName: 'Morgan', lastName: 'Fogarty', age: 32 },
+]
+// Write a function called 'getFirstName' that takes one 
+// argument - a person - and returns the person's first name.
+
+// Write a function called 'getAllFirstNames' that takes one
+// argument - an array of people - and returns every person's 
+// first name.
+// * use a for loop 
+// * use array.map
