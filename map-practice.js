b0b8afe182f525bd35bced2aa90f2712258cc681
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-05 10:50:08 -0700

    more practice with map

--- map-practice.js
+++ map-practice.js
@@ -6,18 +6,41 @@ const people = [
     { firstName: 'Morgan', lastName: 'Fogarty', age: 32 },
 ]
 const a = 42;
+
 // Write a function called 'getFirstName' that takes one 
 // argument - a person - and returns the person's first name.
 function getFirstName(person) {
-    console.log(a);
-    // console.log(foo);
     return person.firstName;
 }
-console.log(getFirstName(people[0]));
-console.log(getFirstName(people[1]));
 
 // Write a function called 'getAllFirstNames' that takes one
 // argument - an array of people - and returns every person's 
 // first name.
 // * use a for loop 
+function getAllFirstNames (people) {
+    let result = [];
+    for (var i = 0; i < people.length; i++){
+        let person = people[i];
+        result.push(getFirstName(person));
+    }
+
+    return result;
+}
+console.log('using a for loop', getAllFirstNames(people));
+
+// Write a function called 'getAllFirstNamesWithMap' that takes one
+// argument - an array of people - and returns every person's 
+// first name.
 // * use array.map
+function getAllFirstNamesWithMap (people){
+    return people.map(getFirstName);
+    // same as: 
+    //  return people.map(function (person) {
+    //      return person.firstName;    
+    //  })
+    // or:
+    //  return people.map(person => person.firstName);
+    // or:
+    //  return people.map(person => getFirstName(person));
+}
+console.log('using array.map', getAllFirstNamesWithMap(people));
