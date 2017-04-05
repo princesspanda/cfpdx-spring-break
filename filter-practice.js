0fecccb2c09a31e1c7037bde8100a84eeb0759f9
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-05 11:14:42 -0700

    adds filter-practice.js

---
+++ filter-practice.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const people = [
+    { firstName: 'Kevin', lastName: 'Wong', age: 21 },
+    { firstName: 'Ivy', lastName: 'Patton', age: 38 },
+    { firstName: 'Morgan', lastName: 'Fogarty', age: 32 },
+]
+
+// Write a function 'isAgeOdd' 
+// arguments: a person
+// returns: true if the person's age is an odd number, false otherwise
+
+// Write a function 'getPeopleWithOddAges'
+// arugments: an array of people
+// returns: an array of people whose ages are odd numbers
