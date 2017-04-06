1d08dc39b25379dbd4d22e3e2bce6c4aa0609b2a
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-06 10:06:11 -0700

    begins reduce-practice

---
+++ reduce-practice.js
@@ -0,0 +1,17 @@
+const characters = [
+    {
+        name: 'Darth Vader',
+        team: 'empire'
+    },
+    {
+        name: 'Han Solo',
+        team: 'rebels'
+    },
+    {
+        name: 'Princess Leia',
+        team: 'rebels'
+    },
+    {
+        name: 'Yoda',
+        team: 'rebels'
+    }]
