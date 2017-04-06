665e9961e66b341fd895cf56b40d4e5e04678ce2
cmd-kvn <kvn.m.wong@gmail.com>
2017-04-06 10:26:55 -0700

    reduce example to create sum

--- reduce-practice.js
+++ reduce-practice.js
@@ -1,17 +1,25 @@
 const characters = [
     {
         name: 'Darth Vader',
-        team: 'empire'
+        team: 'Empire'
     },
     {
         name: 'Han Solo',
-        team: 'rebels'
+        team: 'Rebels'
     },
     {
         name: 'Princess Leia',
-        team: 'rebels'
+        team: 'Rebels'
     },
     {
         name: 'Yoda',
-        team: 'rebels'
+        team: 'Rebels'
     }]
+
+let example1 = [1,2,3].reduce(function(sum, currentVal){ // 'sum' is often called 'accumulator' or 'previous', 'currentVal' is often called 'next'
+   sum += currentVal;
+   return sum
+}, 4); // 4 is the initial value; it becomes 0 if it's not listed
+
+console.log(example1) // 10; 4 + 1 + 2 + 3
+
